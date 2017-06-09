c63c144cc854ec15200fb3fb55369d4b82211764
whling <1769778747@qq.com>
2017-06-09 12:46:24 +0800

    version 1.0 is done

---
+++ src/com/whl/cms/test/CMSTest.java
@@ -0,0 +1,8 @@
+package com.whl.cms.test;
+
+public class CMSTest {
+	public static void main(String[] args) {
+		System.out.println("version 1.0 is devloping......");
+		System.out.println("version 1.0 is done");
+	}
+}

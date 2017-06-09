6712388eec22c5027cfb6f4ef481dfb0d5c51e20
whling <1769778747@qq.com>
2017-06-09 13:26:30 +0800

    devloping v2.0

--- src/com/whl/cms/test/CMSTest.java
+++ src/com/whl/cms/test/CMSTest.java
@@ -4,5 +4,7 @@ public class CMSTest {
 	public static void main(String[] args) {
 		System.out.println("version 1.0 is devloping......");
 		System.out.println("version 1.0 is done");
+
+		System.out.println("version 2.0 is devloping......");
 	}
 }

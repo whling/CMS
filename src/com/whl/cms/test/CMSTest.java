eed3d6bd07d1a4cc8ff6a5592024f6e41734b4ce
whling <1769778747@qq.com>
2017-06-09 13:25:15 +0800

    fixed v1.0

--- src/com/whl/cms/test/CMSTest.java
+++ src/com/whl/cms/test/CMSTest.java
@@ -4,5 +4,6 @@ public class CMSTest {
 	public static void main(String[] args) {
 		System.out.println("version 1.0 is devloping......");
 		System.out.println("version 1.0 is done");
+		System.out.println("version 1.0 had fixed");
 	}
 }
